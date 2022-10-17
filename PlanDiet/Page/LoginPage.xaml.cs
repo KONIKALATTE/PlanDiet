00340e68ec57496c55de62a001b30607461fbadf
Konikalatte <user@DESKTOP-NEJUVLU>
2022-10-17 16:10:37 -0700

    addd delete cancle btn done

--- PlanDiet/Page/LoginPage.xaml.cs
+++ PlanDiet/Page/LoginPage.xaml.cs
@@ -11,31 +11,32 @@ public partial class LoginPage : ContentPage
 	}
     private async void Btnsignin_Clicked(object sender, EventArgs e)
     {
-        progressLoading.IsVisible = true;
-        var a = await R_users.UserLogin(entryEmail.Text, entryPassword.Text);
-        if (entryEmail.Text == null)
-        {
-            await DisplayAlert("Warning", "Enter your email", "ok!");
-            return;
-        }
-        else if (entryPassword.Text == null)
-        {
-            await DisplayAlert("Warning", "Enter your password", "ok!");
-            return;
-        }
-        else if (a)
-        {
-            await DisplayAlert("Warning", "Login Successfully", "ok!");
+        
+            progressLoading.IsVisible = true;
+            var a = await R_users.UserLogin(entryEmail.Text, entryPassword.Text);
+            if (entryEmail.Text == null)
+            {
+                await DisplayAlert("Warning", "Enter your email", "ok!");
+                return;
+            }
+            else if (entryPassword.Text == null)
+            {
+                await DisplayAlert("Warning", "Enter your password", "ok!");
+                return;
+            }
+            else if (a)
+            {
+                await DisplayAlert("Warning", "Login Successfully", "ok!");
+                Application.Current!.MainPage = new AppShell();
+                return;
+            }
+            await DisplayAlert("Warning", "Please try again", "ok!");
 
-            return;
-        }
-        await DisplayAlert("Warning", "Please try again", "ok!");
 
-        Application.Current!.MainPage = new AppShell();
-        progressLoading.IsVisible = false;
-    }
-    private async void Btncancel_Clicked(object sender, EventArgs e)
+            progressLoading.IsVisible = false;
+        }
+        private async void Btncancel_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new startup());
+        await Navigation.PopAsync();
     }
 }
